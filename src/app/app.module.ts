182ac2aebcc042cb1acbd13c1003cfe99927df4a
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-06-08 21:56:35 +0300

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NewsListComponent } from './news/components/news-list/news-list.component';
+import { NewsDetailComponent } from './news/components/news-detail/news-detail.component';
+import { NewsModule } from './news/news.module';
+
+const routesConfig: Routes = [
+  { path: '', redirectTo: '/news', pathMatch: 'full' },
+  { path: 'news', component: NewsListComponent },
+  { path: 'news/:id', component: NewsDetailComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NewsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routesConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
